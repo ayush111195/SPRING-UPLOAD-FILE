9a2b477456b32adeca22961f061f460b7b8bb9a1
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-09-24 20:04:19 +0530

    Update Controller.java

--- springmvcform/src/main/java/springmvcform/controller/Controller.java
+++ springmvcform/src/main/java/springmvcform/controller/Controller.java
@@ -3,7 +3,7 @@ package springmvcform.controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation. RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import springmvcform.controller.model.Student;
